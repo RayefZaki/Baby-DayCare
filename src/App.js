894f36524e4d87b93cafa1d7ffb9357941e0ad7d
Nawaf <nawaftalaat@hotmail.com>
2022-11-07 16:31:21 +0300

    Creating card and search

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import Navbar from './Components/Navbar'
 import { ChakraProvider } from '@chakra-ui/react'
 import Footer from './Components/Footer'
 import Create from './Components/Create';
+import Search from './Components/Search';
 
 function App() {
   return (
     <div className="App">
 
-      <h2>hjhjhjh</h2>
-      <Create></Create>
+      {/* <h2>hjhjhjh</h2>
+      <Create></Create> */}
+      <Search />
 
     </div>
 
