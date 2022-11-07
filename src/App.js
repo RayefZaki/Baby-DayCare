87889b2f7f7146624aa1dccba3310b947b77cf0c
majed <m.a.a.almaged@gmail.com>
2022-11-07 15:39:36 +0300

    ADD Sign up whit out function, next modify will be Added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import Navbar from './Components/Navbar'
 import { ChakraProvider } from '@chakra-ui/react'
 import Footer from './Components/Footer'
 import Create from './Components/Create';
+import Signup from './Components/Signup'
 
 function App() {
   return (
     <div className="App">
-
+<Navbar/>
       <h2>hjhjhjh</h2>
       <Create></Create>
+<Signup/>
+     
 
     </div>
 
