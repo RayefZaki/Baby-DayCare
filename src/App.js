59febbb86294bcdf975c2f40bb3bdbe7db65d490
RaZaki <Rayef.zaki@gmail.com>
2022-11-07 12:22:17 +0300

    Add Create

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Create from './Components/Create';
 
 function App() {
   return (
     <div className="App">
-      
+      <h2>hjhjhjh</h2>
+      <Create></Create>
 
     </div>
   );
---
+++ src/Components/Create.js
@@ -0,0 +1,40 @@
+import React, {useState} from 'react'
+import axios from 'axios'
+// import {useNavigate} from "react-router-dom"
+
+function Create() {
+    const[course,setCourse]=useState()
+    const[shortDesc,setShortDesc]=useState()
+    const[description,setDescription]=useState()
+    const[city,setCity]=useState()
+    
+    let url = "https://6362424b7521369cd068e00e.mockapi.io/DayCare"
+
+    // let navigate = useNavigate();
+    const postData=()=>{
+        axios.post(url,{
+            course,description,city,shortDesc
+
+        }).then(res =>{
+            console.log(res)
+
+        })
+        // navigate('/GitCreate');
+    }
+  return (
+    <div>
+
+
+        <input placeholder='Course Name' onChange={e=>{setCourse(e.target.value) }}></input>
+        <input placeholder='Short Description' onChange={e=>{setShortDesc(e.target.value) }}></input>
+        <input placeholder='Description' onChange={e=>{setDescription(e.target.value) }}></input>
+        <input placeholder='City' onChange={e=>{setCity(e.target.value) }}></input>
+        <button onClick={postData}>Add New Course</button>
+
+
+
+    </div>
+  )
+}
+
+export default Create
